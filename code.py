3d3a1810c1b23528f5f604d33011c4beb50f0d3b
Gergo Gyori <gergo.gyori@gmail.com>
2023-02-24 18:29:44 +0100

    online - trial

---
+++ code.py
@@ -0,0 +1,53 @@
+import utils as m
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Gergo Gyoir's Bachelor Project", page_icon="🎄", initial_sidebar_state="collapsed"
+)
+st.markdown("# Check my code")
+
+st.write("")
+form = st.form(key="form_settings")
+radiusPubs, radiusFuel, radiusSP = form.columns([1, 1, 1])
+
+radiusPubs = col1.slider(
+    "Radius of Pubs",
+    1,
+    100000,
+    key="radiusPubsK",
+)
+
+radiusFuel = col1.slider(
+    "Radius of Fuel",
+    1,
+    100000,
+    key="radiusFuelK",
+)
+
+radiusSP = col1.slider(
+    "Radius of Supermarkets",
+    1,
+    100000,
+    key="radiusSPK",
+)
+
+with st.spinner("Creating map... (may take up to a minute)"):
+    # loading
+    None
+
+
+if st.button('Calculation'):
+    gdf1 = m.openJSON(filename="rawpPoints/pubs.json")
+    polyg1 = m.polygonMakerShapely(gdf1, diameter=radiusPubs, numberOfPoints=50)
+    polyg1 = polyg1.geometry.unary_union
+    multi1 = m.gpd.GeoSeries([polyg1])
+    st.write(len(multi1))
+    # do the calculation
+else:
+    st.write('Goodbye')
+
+#
+
+
+# transaction_df = transaction_df[transaction_df["list_price"] > standard_cost_slider
---
+++ utils.py
@@ -0,0 +1,41 @@
+def openJSON(filename=""):
+    """
+    open geopandas file from json object
+    """
+    file = open(filename)
+    fileR = json.load(file)
+    gdf = gpd.GeoDataFrame.from_features(fileR["features"])
+    # gdf.crs = "EPSG:23700" #Hungary, but doesnt wokr when I wanna explore
+    gdf.crs = "epsg:4326"
+    file.close()
+    # cleaning
+    del fileR
+    return gdf
+
+def polygonMakerShapely(datab, diameter, numberOfPoints=100):
+    """ 
+    Make a sircle around a point.
+    Note: It make a circle around the middle of polygons, not around the edges of the building
+    """
+    polygon = [] # collect polygons
+    angles = np.linspace(0, 360, numberOfPoints) # calculate circle area around a point
+    datab.crs = "epsg:4326"
+    datab = datab.to_crs(crs=4326)
+    # !!!!!!!!!!!!!!!! totally different
+    datab["geometry"] = datab.to_crs('+proj=cea').centroid.to_crs(datab.crs) # get the center of all type geometries
+    for coordinates in datab["geometry"]:
+        poly = geometry.Polygon(geog.propagate([coordinates.x, coordinates.y], angles, diameter))
+        polygon.append(poly)
+    # new 19.02.2023 -- since we return the whole db, it is slower. 
+    datab = gpd.GeoDataFrame(geometry=polygon)
+    datab.crs = "epsg:4326"
+    # datab["geometry"] = polygon
+    return datab
+
+
+import json
+import geopandas as gpd
+import numpy as np
+# !pip install geog
+import geog
+from shapely import geometry
